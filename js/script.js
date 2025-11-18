642888c94297606b373f19ec6b63b407143e89d8
Carmohere <lclucianocarmo24@gmail.com>
2025-11-17 22:04:13 -0300

    a

--- js/script.js
+++ js/script.js
@@ -397,20 +397,3 @@ function createConfetti() {
     }, i * 30);
   }
 }
-
-(function () {
-  const KEY = 'quizIgualdadeViews';
-
-  let count = Number(localStorage.getItem(KEY) || 0);
-  if (Number.isNaN(count)) count = 0;
-
-  count += 1;
-  localStorage.setItem(KEY, String(count));
-
-  const span = document.getElementById('viewCount');
-  if (span) {
-    span.textContent = count;
-  } else {
-    console.warn('Elemento #viewCount não encontrado no DOM.');
-  }
-})();
